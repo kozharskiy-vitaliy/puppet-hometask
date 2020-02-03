2a1b4d2173c26dafccacae45377af501344c0eaf
kozharskiy-vitaliy <kozharskiy.vitaliy@gmail.com>
2020-02-03 23:54:34 +0300

    Puppet commit 3

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,17 +2,51 @@ node default {
 }
 
 node 'slave1.puppet' {
-   include role::slave1
+   class { 'apache': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.html':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/kozharskiy-vitaliy/puppet-hometask/production/files/index.html',
+      replace => false,
+      }
 }
 
 node 'slave2.puppet' {
-   include role::slave2
+   class { 'apache::mod::php': }
+   
+   class { 'php': }
+   
+   file { '/root/README':
+      ensure => absent,
+      }
+   
+   file { '/var/www/html/index.php':
+      ensure => file,
+      source => 'https://raw.githubusercontent.com/kozharskiy-vitaliy/puppet-hometask/production/files/index.php',
+      replace => false,
+      }
 }
 
 node 'master.puppet' {
-   include role::master
+   include nginx
+
+   nginx::resource::server { '192.168.1.1:80':
+      ensure      => present,
+      listen_port => 80,
+      proxy       => 'http://192.168.1.3:80',
+   }
+
+   nginx::resource::server { '192.168.1.1:81':
+      ensure      => present,
+      listen_port => 81,
+      proxy       => 'http://192.168.1.3:80',
+   }
 }
 
 node 'mineserver.puppet' {
-   include role::mineserver
+   include mineserver
 }
